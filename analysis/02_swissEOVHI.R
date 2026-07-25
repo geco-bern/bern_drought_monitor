a49c3a4dd941e26e4c4d4eb9aa79269239f59e57
fabern <10245680+fabern@users.noreply.github.com>
2026-07-25 09:05:56 +0200

    show larger VHI map and more than 1 date (store a tif instead of csv)

--- analysis/02_swissEOVHI.R
+++ analysis/02_swissEOVHI.R
@@ -16,6 +16,7 @@ library(patchwork)
 library(dplyr)
 library(lubridate)
 library(scales)
+library(here)
 
 # ---------------------------
 # Settings
@@ -23,30 +24,8 @@ library(scales)
 stac_root  <- "https://data.geo.admin.ch/api/stac/v0.9"
 collection <- "ch.swisstopo.swisseo_vhi_v100"
 
-# Point of interest, WGS84 / EPSG:4326
-# Input order here: longitude, latitude
-poi_lon <- 7.435386672754767
-poi_lat <- 46.949013442083604
-
-# Radius around the point in meters
-buffer_m <- 5000
-
-# Create a circular area around the point
-poi <- st_as_sf(
-  data.frame(id = 1, lon = poi_lon, lat = poi_lat),
-  coords = c("lon", "lat"),
-  crs = 4326
-)
-
-aoi_lv95 <- poi |>
-  st_transform(2056) |>
-  st_buffer(dist = buffer_m)
-
-# Bounding box for STAC search, transformed back to WGS84
-bbox_wgs84 <- aoi_lv95 |>
-  st_transform(4326) |>
-  st_bbox() |>
-  as.numeric()
+# Region of interest
+bbox_wgs84 <- c(7.1, 46.9, 7.53, 47.19)
 
 # Output folder
 out_dir <- here("data-raw","swisseo_vhi_bern")
@@ -203,46 +182,73 @@ latest_date <- as.Date(item_datetime(latest_feature))
 
 message("Latest valid VHI dataset: ", latest_date)
 
+# 1b) Find the latest three available datasets
+latest_feature_2 <- find_latest_valid_feature_by_day(
+  start_date = latest_date - 1,
+  days_back = 180,
+  layer = vhi_layer
+)
+latest_date_2 <- as.Date(item_datetime(latest_feature_2))
+latest_feature_3 <- find_latest_valid_feature_by_day(
+  start_date = latest_date_2 - 1,
+  days_back = 180,
+  layer = vhi_layer
+)
+latest_date_3 <- as.Date(item_datetime(latest_feature_3))
+
+
 # ---------------------------
 # 2) Read remote asset and crop it to the AOI
 # ---------------------------
 latest_href <- pick_tif_asset(latest_feature, layer = vhi_layer)
-
 latest_crop <- crop_to_aoi_from_url(latest_href)
 
-# Save the cropped GeoTIFF
-# writeRaster(
-  # latest_crop,
-  #file.path(out_dir, paste0("swisseo_vhi_bern_crop_", latest_date, ".tif")),
-  #overwrite = TRUE
-# )
+latest_href_2 <- pick_tif_asset(latest_feature_2, layer = vhi_layer)
+latest_crop_2 <- crop_to_aoi_from_url(latest_href_2)
+
+latest_href_3 <- pick_tif_asset(latest_feature_3, layer = vhi_layer)
+latest_crop_3 <- crop_to_aoi_from_url(latest_href_3)
+
+# TODO: create hardcoded swisseo_vhi_bern_plot_data_July16.tif
+
 
 # ---------------------------
-# 3) Save processed data for vignette
+# 4) Process for plotting in vignette
 # ---------------------------
+raster_stack <- c(latest_crop, latest_crop_2, latest_crop_3)
+time(raster_stack) <- c(latest_date, latest_date_2, latest_date_3)
+
+# VHI auf gültigen Bereich begrenzen
+dim(raster_stack)
+
+
+seq_len(dim(raster_stack)[[3]])
+for (i in seq_len(nlyr(raster_stack))) {
+  idx <- raster_stack[[i]] < 0 | raster_stack[[i]] > 100
+  raster_stack[[i]][idx] <- NA
+}
+# Für leaflet nach WGS84 transformieren
+raster_stack_wgs84 <- terra::project(raster_stack, "EPSG:4326", method = "near")
+
+
+# ---------------------------
+# 4) Save processed data for vignette
+# ---------------------------
+
 
 dir.create("data-dynamic", showWarnings = FALSE)
-
-# Save cropped raster
+# Save cropped raster stack
 writeRaster(
-  latest_crop,
-  here("data-dynamic", paste0("swisseo_vhi_bern_crop_", latest_date, ".tif")),
+  raster_stack_wgs84,
+  here("data-dynamic", paste0("swisseo_vhi_bern_plot_data_3dates.tif")),
   overwrite = TRUE
 )
 
-# Save plot-ready data frame
+# Save summary statistics
 plot_df <- raster_to_df(
   latest_crop,
   paste0(latest_date, " (latest valid available)")
 )
-
-write.csv(
-  plot_df,
-  here("data-dynamic", "swisseo_vhi_bern_plot_data.csv"),
-  row.names = FALSE
-)
-
-# Save summary statistics
 stats <- plot_df |>
   summarise(
     date = unique(date),
